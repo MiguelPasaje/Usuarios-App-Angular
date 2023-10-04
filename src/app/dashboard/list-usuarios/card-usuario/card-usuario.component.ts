e523893624fa07f16cf715828411bf4b6dcf1bce
MiguelPasaje <cartel-dy@hotmail.com>
2023-10-04 10:43:03 -0500

    avances

---
+++ src/app/dashboard/list-usuarios/card-usuario/card-usuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-usuario',
+  templateUrl: './card-usuario.component.html',
+  styleUrls: ['./card-usuario.component.css']
+})
+export class CardUsuarioComponent {
+  imgUrl:string = 'https://www.fayerwayer.com/resizer/j5t99xUsygsnO8VAhqzTEvzC9l8=/800x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/55QHEOQRQBEPTF5LRJK57MDEP4.jpg'
+
+}
