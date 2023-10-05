125ffbc9ef555955b39c616e9cc64557d90c03f8
MiguelPasaje <cartel-dy@hotmail.com>
2023-10-04 21:35:04 -0500

    avances

--- src/app/dashboard/list-usuarios/card-usuario/card-usuario.component.ts
+++ src/app/dashboard/list-usuarios/card-usuario/card-usuario.component.ts
@@ -11,11 +11,12 @@ export class CardUsuarioComponent implements OnInit {
   imgUrl: string = 'https://www.fayerwayer.com/resizer/j5t99xUsygsnO8VAhqzTEvzC9l8=/800x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/55QHEOQRQBEPTF5LRJK57MDEP4.jpg';
   firstName: string = '';
   email: string = '';
+  id:number = 0
 
   ngOnInit(): void {
     this.firstName = this.user.name;
     this.email = this.user.email;
-    console.log(this.imgUrl,this.firstName,this.email);
-
+    this.id = this.user.id
+    console.log(this.id,this.firstName,this.email);
   }
 }
--- src/app/dashboard/usuario/usuario.component.ts
+++ src/app/dashboard/usuario/usuario.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
-  styleUrls: ['./usuario.component.css']
+  styleUrls: ['./usuario.component.css'],
 })
-export class UsuarioComponent {
+export class UsuarioComponent implements OnInit {
+  id: number;
+  Name: string = '';
+  email: string = '';
+  urlImg: string = '';
+  phone: string = '';
+  gender: string = '';
+  loading:boolean = true
+  constructor(
+    private aRoute: ActivatedRoute,
+    private userService: UsuarioService
+  ) {
+    this.id = +aRoute.snapshot.paramMap.get('id')!;
+  }
+  ngOnInit(): void {
+    console.log(this.id);
+    this.getUsuario();
+  }
 
+  getUsuario(): void {
+    this.userService.getUsuario(this.id).subscribe((res) => {
+      console.log(res.data);
+      this.Name = res.data.name
+      this.email = res.data.email
+      this.gender = res.data.gender
+      console.log(this.Name,this.email,this.gender);
+      this.loading = false
+
+    });
+  }
 }
